0d929198810c07c94637a2b597ac492acad3e599
M <2407098@cognizant.com>
2025-05-21 10:23:30 +0530

    Basic folder Structure

---
+++ src/main/java/com/example/VehicleParkingManagementSystem/VehicleParkingManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.VehicleParkingManagementSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehicleParkingManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VehicleParkingManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/VehicleParkingManagementSystem/VehicleParkingManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.VehicleParkingManagementSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehicleParkingManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
